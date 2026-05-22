49297179d1e80067a41426483112a5cbdbf74c1d
AntonKashichkin <Anton.Kashichkin@onlyoffice.com>
2026-05-22 15:42:26 +0300

    Fixed crashed tests.

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ dotenv.config();
 export default defineConfig({
   testDir: "./src/tests",
   retries: process.env.CI ? 1 : 0,
-  workers: 1,
+  workers: 4,
 
   outputDir: `./test-output/${process.env.JOB_NAME ?? "local"}`,
   reporter: [
