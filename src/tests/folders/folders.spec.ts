c4214956c85bb41b88fd2151208d1bb775aa5779
AnnaG0ncharenko <anna.goncharenko@onlyoffice.com>
2026-05-29 13:59:38 +0300

    Fix  error TS2559: Type 'string' has no properties in common with type 'ApiDateTime'.

--- src/tests/folders/folders.spec.ts
+++ src/tests/folders/folders.spec.ts
@@ -6610,7 +6610,11 @@ test.describe("GET /api/2.0/files/folder/{id}/link - Get folder primary external
 
     await ownerApi.folders.setFolderPrimaryExternalLink({
       id: roomId,
-      folderLinkRequest: { linkId, access: FileShare.Read, expirationDate },
+      folderLinkRequest: {
+        linkId,
+        access: FileShare.Read,
+        expirationDate: expirationDate as any,
+      },
     });
 
     const { data, status } =
